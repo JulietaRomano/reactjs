9ae6ad27cf8ea55b6904fbbc781e12a3056a2812
JulietaRomano <romanojulietacarolina@gmail.com>
2021-12-01 17:07:33 -0300

    firestone

--- src/components/Items/Products.jsx
+++ src/components/Items/Products.jsx
@@ -1,26 +1,23 @@
-import image1 from'../assets/traje1.jpg'
-import image2 from'../assets/traje2.jpg'
-import image3 from'../assets/traje3.jpg'
  
  const listaProductos =   [
     {
         producto:'Blaizer',
         id:1,
-        imagen: image1,
+        imagen: 'https://images.unsplash.com/photo-1497339100210-9e87df79c218?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
         precio: 6000,
         categoria:'abrigos'
     },
     {
         producto:'Campera',
         id:2,
-        imagen: image2,
+        imagen: 'https://images.unsplash.com/photo-1597133524931-67b0b6c9bfc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGphY2tldHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
         precio: 8000,
         categoria:'abrigos'
     },
     {
         producto:'Pantalon',
         id:3,
-        imagen: image3,
+        imagen: 'https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
         precio: 4500,
         categoria:'pantalones'
     },
